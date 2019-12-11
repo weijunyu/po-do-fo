17284769e7327ac9e486d3b0312c967dc72b21e6
Wei Junyu <weijunyu@users.noreply.github.com>
2019-12-11 22:08:36 +0800

    Added uppy for loading files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import Uppy from "@uppy/core";
+import Dashboard from "@uppy/dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+import "@uppy/core/dist/style.css";
+import "@uppy/dashboard/dist/style.css";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    uppy: null
+  };
+  componentDidMount() {
+    let uppyInstance = Uppy().use(Dashboard, {
+      target: ".uploader",
+      inline: true
+    });
+    this.setState({
+      uppy: uppyInstance
+    });
+    uppyInstance.on("file-added", file => {
+      let fileRef = file.data;
+      let reader = new FileReader();
+      reader.onload = event => {
+        
+      }
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="uploader"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
