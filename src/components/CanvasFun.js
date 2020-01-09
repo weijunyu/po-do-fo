5bd40d9704f7bb9e7ee60972134f26420c7bb1f3
Wei Junyu <weijunyu@users.noreply.github.com>
2020-01-09 23:21:08 +0800

    Can finally draw things

--- src/components/CanvasFun.js
+++ src/components/CanvasFun.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./CanvasFun.css";
+
 function CanvasFun() {
-  const canvasRef = React.createRef();
+  const [canvasRef] = useState(React.createRef());
   const [painting, setPainting] = useState(false);
+  const [canvasCtx, setCanvasCtx] = useState(null);
+  const [mouseCoordinates, setMouseCoordinates] = useState([]);
+  const [canvasPosition, setCanvasPosition] = useState({});
+
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
+    setCanvasCtx(ctx);
 
     // Resizing
     canvas.height = 768;
@@ -23,22 +29,62 @@ function CanvasFun() {
     ctx.lineTo(175, 130);
     ctx.closePath();
     ctx.stroke();
+    ctx.beginPath();
   }, [canvasRef]);
+
+  useEffect(() => {
+    const rect = canvasRef.current.getBoundingClientRect();
+    setCanvasPosition(rect);
+    function onScroll() {
+      const rect = canvasRef.current.getBoundingClientRect();
+      setCanvasPosition(rect);
+    }
+    window.addEventListener("scroll", onScroll, true);
+    return function cleanup() {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [canvasRef]);
+
   function startPainting() {
     setPainting(true);
   }
   function stopPainting() {
     setPainting(false);
+    canvasCtx.beginPath();
   }
+  function paint(e) {
+    setMouseCoordinates([e.clientX, e.clientY]);
+    if (!painting) return;
+    canvasCtx.lineWidth = 10;
+    canvasCtx.lineCap = "round";
+    canvasCtx.lineTo(
+      e.clientX - canvasPosition.x,
+      e.clientY - canvasPosition.y
+    ); // Go to mouse
+    canvasCtx.stroke();
+    canvasCtx.beginPath();
+    canvasCtx.moveTo(
+      e.clientX - canvasPosition.x,
+      e.clientY - canvasPosition.y
+    );
+  }
+  function getMousePosition() {}
+
   return (
     <div className="canvas-fun">
       <h1>Canvas below: {painting}</h1>
-      <canvas
-        id="my-canvas"
-        ref={canvasRef}
-        onMouseDown={startPainting}
-        onMouseUp={stopPainting}
-      ></canvas>
+      <div className="canvas-container">
+        <canvas
+          id="my-canvas"
+          ref={canvasRef}
+          onMouseDown={startPainting}
+          onMouseUp={stopPainting}
+          onMouseMove={paint}
+        ></canvas>
+        <span>
+          {mouseCoordinates[0]},{mouseCoordinates[1]}
+        </span>
+      </div>
     </div>
   );
 }
