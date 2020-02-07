41f2ab6d425b6a193ec62bd85d650724dcb6b10c
Wei Junyu <weijunyu@users.noreply.github.com>
2020-02-08 00:50:28 +0800

    Removed canvas editing functions. Updated packages.

--- src/components/DocumentFrame.js
+++ src/components/DocumentFrame.js
@@ -6,86 +6,12 @@ import "./DocumentFrame.css";
 class DocumentFrame extends React.Component {
   constructor(props) {
     super(props);
-    this.pageRef = React.createRef();
     this.state = {};
     this.state.blob = new Blob([this.props.pageBytes], {
       type: "application/pdf"
     });
     this.state.blobUrl = URL.createObjectURL(this.state.blob);
-    this.state.canvasPosition = {};
-    this.state.painting = false;
-    this.state.mouseCoordinates = [];
   }
-
-  onDocumentLoad = pdf => {
-    if (this.props.editable) {
-      this.enableEditing();
-    }
-  };
-
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.onScroll);
-  }
-
-  enableEditing = () => {
-    let pageWrapper = this.pageRef.current.ref;
-    let canvasElement = pageWrapper.querySelector(`canvas`);
-    this.canvasElement = canvasElement;
-
-    this.setState({
-      canvasCtx: canvasElement.getContext("2d")
-    });
-
-    const rect = canvasElement.getBoundingClientRect();
-    this.setState({
-      canvasPosition: rect
-    });
-
-    window.addEventListener("scroll", this.onScroll);
-
-    canvasElement.addEventListener("mousedown", e => {
-      this.setState({
-        painting: true
-      });
-      this.paint(e, true);
-    });
-
-    canvasElement.addEventListener("mouseup", e => {
-      this.setState({
-        painting: false
-      });
-      this.state.canvasCtx.beginPath();
-    });
-
-    canvasElement.addEventListener("mousemove", e => this.paint(e));
-  };
-
-  onScroll = () => {
-    const rect = this.canvasElement.getBoundingClientRect();
-    this.setState({
-      canvasPosition: rect
-    });
-  };
-
-  paint = (e, force = false) => {
-    this.setState({ mouseCoordinates: [e.clientX, e.clientY] });
-    let painting = this.state.painting;
-    let canvasCtx = this.state.canvasCtx;
-    let canvasPosition = this.state.canvasPosition;
-    if (!painting && !force) return;
-    canvasCtx.lineWidth = 10;
-    canvasCtx.lineCap = "round";
-    canvasCtx.lineTo(
-      e.clientX - canvasPosition.x,
-      e.clientY - canvasPosition.y
-    ); // Go to mouse
-    canvasCtx.stroke();
-    canvasCtx.beginPath();
-    canvasCtx.moveTo(
-      e.clientX - canvasPosition.x,
-      e.clientY - canvasPosition.y
-    );
-  };
 
   render() {
     return (
@@ -94,28 +20,20 @@ class DocumentFrame extends React.Component {
           file={this.state.blobUrl}
           className={`${this.props.className}-doc`}
         >
-          <Page
-            pageNumber={1}
-            onRenderSuccess={this.onDocumentLoad}
-            className={this.props.className}
-            ref={this.pageRef}
-          />
+          <Page pageNumber={1} className={this.props.className} />
         </Document>
       </div>
     );
   }
 }
-DocumentFrame.defaultProps = {
-  editable: false
-};
+
 DocumentFrame.propTypes = {
   pageBytes: function(props, propName) {
     return props[propName] instanceof Uint8Array
       ? null
       : new Error("pageBytes should be a TypedArray.");
   },
-  className: PropTypes.string,
-  editable: PropTypes.bool
+  className: PropTypes.string
 };
 
 export default DocumentFrame;
--- src/components/EditorView.js
+++ src/components/EditorView.js
@@ -1,99 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { Redirect, useParams } from "react-router-dom";
-import { PDFDocument } from "pdf-lib";
 
-import { setPage } from "../redux/actions";
 import DocumentFrame from "./DocumentFrame";
 
 import "./EditorView.css";
 
 function EditorView(props) {
-  let documentFrameRef = React.createRef();
-  const [showTextBox, setShowTextBox] = useState(false);
-  const [cursorLocation, setCursorLocation] = useState({});
-  const [textBoxLocation, setTextBoxLocation] = useState({});
-  const [isPlacingTextBox, setIsPlacingTextBox] = useState(false);
   const routeParams = useParams();
   const pageIndex = parseInt(routeParams.pageIndex);
-
-  useEffect(() => {
-    function recordMousePosition(e) {
-      setCursorLocation({ x: e.clientX, y: e.clientY });
-    }
-    document.addEventListener("mousemove", recordMousePosition);
-    return function() {
-      document.removeEventListener("mousemove", recordMousePosition);
-    };
-  }, []);
 
   if (props.pages.length === 0) {
     return <Redirect to="/" />;
   }
 
-  async function saveCanvasAsPdf() {
-    let canvas = document.querySelector(`.page-${pageIndex} canvas`);
-    let newDoc = await PDFDocument.create();
-    let imgData = canvas.toDataURL("image/jpeg", 1.0);
-    let pdfImage = await newDoc.embedJpg(imgData);
-    const page = newDoc.addPage();
-    page.drawImage(pdfImage);
-    const pdfBytes = await newDoc.save();
-    props.setPage(pageIndex, pdfBytes);
-  }
-
-  function onViewClick(e) {
-    if (isPlacingTextBox) {
-      let editorViewEl = document.querySelector(`.editor-view`);
-      // if e.target is within canvas, set absolute x and y position values for text box.
-      const rect = editorViewEl.getBoundingClientRect();
-      setTextBoxLocation({
-        x: e.clientX - rect.x,
-        y: e.clientY - rect.y
-      });
-      setShowTextBox(true);
-      setIsPlacingTextBox(false);
-    } else if (showTextBox) {
-      if (e.target === document.querySelector(".text-input-overlay")) return;
-      setShowTextBox(false);
-    }
-  }
-
   return (
-    <div className="editor-view" onClick={onViewClick}>
+    <div className="editor-view">
       <DocumentFrame
         pageBytes={props.pages[pageIndex].bytes}
         className={`page-${pageIndex}`}
         editable={true}
-        ref={documentFrameRef}
-        style={{ cursor: isPlacingTextBox ? "crosshair" : "auto" }}
       />
-      {showTextBox ? (
-        <TextInput
-          close={() => setShowTextBox(false)}
-          location={textBoxLocation}
-        />
-      ) : null}
       <div className="editor-controls">
-        <button className="btn" onClick={() => setIsPlacingTextBox(true)}>
-          Add Text
-        </button>
-        <button className="btn success" onClick={saveCanvasAsPdf}>
-          Save
-        </button>
+        <button className="btn success">Save</button>
       </div>
-    </div>
-  );
-}
-
-function TextInput({ close, location }) {
-  useEffect(() => {
-    document.querySelector(".text-input-overlay").focus();
-  }, []);
-  return (
-    <div style={{ position: "absolute", left: location.x, top: location.y }}>
-      <input className="text-input-overlay" type="text" />
-      <button onClick={close}>OK</button>
     </div>
   );
 }
@@ -101,7 +31,4 @@ function TextInput({ close, location }) {
 function mapStateToProps(state) {
   return { pages: state.pages };
 }
-const mapDispatchToProps = {
-  setPage
-};
-export default connect(mapStateToProps, mapDispatchToProps)(EditorView);
+export default connect(mapStateToProps)(EditorView);
