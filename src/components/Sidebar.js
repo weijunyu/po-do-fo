59254426a59fef55bae7917fbaf314a5c1cdaf10
Wei Junyu <weijunyu@users.noreply.github.com>
2020-05-05 22:32:56 +0800

    Moved export functions to lib

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { useRouteMatch, Link } from "react-router-dom";
-import { PDFDocument } from "pdf-lib";
-import { saveAs } from "file-saver";
 import Uppy from "@uppy/core";
 import DragDrop from "@uppy/drag-drop";
 
 import { loadPagesFromFile } from "../redux/actions";
+import { exportPdf, exportPdfInImages } from "../lib";
 
 import "@uppy/core/dist/style.css";
 import "@uppy/drag-drop/dist/style.css";
@@ -42,47 +41,15 @@ function PdfLoader(props) {
     });
   }, [loadPagesFromFile]);
 
-  async function exportPdf() {
-    let newDocument = await PDFDocument.create();
-    for (let page of props.pages) {
-      let pageDoc = await PDFDocument.load(page.bytes);
-      const [pdfPage] = await newDocument.copyPages(pageDoc, [0]);
-      newDocument.addPage(pdfPage);
-    }
-    let final = await newDocument.save();
-    const file = new File([final], "export.pdf", { type: "application/pdf" });
-    saveAs(file);
-  }
-
-  async function exportPdfInImages() {
-    // new pdf doc
-    let newDoc = await PDFDocument.create();
-    let canvases = document.querySelectorAll("canvas.react-pdf__Page__canvas");
-    // for every canvas element,
-    // get image, make new page, copy image to new page
-    for (let canvas of canvases) {
-      let b64CanvasImage = canvas.toDataURL();
-      let pdfImg = await newDoc.embedPng(b64CanvasImage);
-      let newPage = newDoc.addPage([pdfImg.width, pdfImg.height]);
-      newPage.drawImage(pdfImg, {
-        x: 0,
-        y: 0,
-        width: pdfImg.width,
-        height: pdfImg.height,
-      });
-    }
-    // let user download pdf doc
-    let final = await newDoc.save();
-    const file = new File([final], "export.pdf", { type: "application/pdf" });
-    saveAs(file);
-  }
-
   return (
     <>
       <div className="pdf-loader"></div>
       {props.pages.length > 0 ? (
         <>
-          <button className="button is-primary compressed" onClick={exportPdf}>
+          <button
+            className="button is-primary compressed"
+            onClick={() => exportPdf(props.pages)}
+          >
             Export PDF
           </button>
           <button
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,41 @@
+import { PDFDocument } from "pdf-lib";
+import { saveAs } from "file-saver";
+
 export function colorDataToCssAttribute(colorData) {
   return `rgba(${colorData.r}, ${colorData.g}, ${colorData.b}, ${colorData.a})`;
+}
+
+export async function exportPdf(pages) {
+  let newDocument = await PDFDocument.create();
+  for (let page of pages) {
+    let pageDoc = await PDFDocument.load(page.bytes);
+    const [pdfPage] = await newDocument.copyPages(pageDoc, [0]);
+    newDocument.addPage(pdfPage);
+  }
+  let final = await newDocument.save();
+  const file = new File([final], "export.pdf", { type: "application/pdf" });
+  saveAs(file);
+}
+
+export async function exportPdfInImages() {
+  // new pdf doc
+  let newDoc = await PDFDocument.create();
+  let canvases = document.querySelectorAll("canvas.react-pdf__Page__canvas");
+  // for every canvas element,
+  // get image, make new page, copy image to new page
+  for (let canvas of canvases) {
+    let b64CanvasImage = canvas.toDataURL();
+    let pdfImg = await newDoc.embedPng(b64CanvasImage);
+    let newPage = newDoc.addPage([pdfImg.width, pdfImg.height]);
+    newPage.drawImage(pdfImg, {
+      x: 0,
+      y: 0,
+      width: pdfImg.width,
+      height: pdfImg.height,
+    });
+  }
+  // let user download pdf doc
+  let final = await newDoc.save();
+  const file = new File([final], "export.pdf", { type: "application/pdf" });
+  saveAs(file);
 }
